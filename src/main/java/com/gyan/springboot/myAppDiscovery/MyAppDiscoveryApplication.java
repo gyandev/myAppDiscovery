eaf81912e4404f1d3b6edb001166362deb2c025d
Pandey,Gyandev <gyan123edu@gmail.com>
2020-09-14 23:04:04 +0530

    First Commit in GIT

---
+++ src/main/java/com/gyan/springboot/myAppDiscovery/MyAppDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.gyan.springboot.myAppDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyan/springboot/myAppDiscovery/MyAppDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyan.springboot.myAppDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
