921abf91ecbd26a4c62440216a704b39255dfcbd
Pandey,Gyandev <gyan123edu@gmail.com>
2020-09-14 23:31:28 +0530

    Commiting Annotation EnableEurekaServer

--- src/main/java/com/gyan/springboot/myAppDiscovery/MyAppDiscoveryApplication.java
+++ src/main/java/com/gyan/springboot/myAppDiscovery/MyAppDiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.gyan.springboot.myAppDiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MyAppDiscoveryApplication {
 
 	public static void main(String[] args) {
